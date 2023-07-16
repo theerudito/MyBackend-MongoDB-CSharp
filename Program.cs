70718ae56255a289a84a6208353cc1ff0c897b0a
theerudito <erudito.tv@gmail.com>
2023-07-16 08:30:22 -0500

    v2

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
-using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using MyBackend_MongoDB_CSharp.Data;
 using MyBackend_MongoDB_CSharp.Repositories;
 using MyBackend_MongoDB_CSharp.Service;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,14 +17,14 @@ var config = proveedor.GetRequiredService<IConfiguration>();
 
 builder.Services.AddCors(options =>
 {
-  var fontendUrl = config.GetValue<string>("MyFrontend");
+    var fontendUrl = config.GetValue<string>("MyFrontend");
 
-  options.AddDefaultPolicy(builder =>
-  {
-    builder.WithOrigins(fontendUrl)
-    .AllowAnyMethod()
-    .AllowAnyHeader();
-  });
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins(fontendUrl)
+      .AllowAnyMethod()
+      .AllowAnyHeader();
+    });
 });
 
 builder.Services.AddControllers();
@@ -32,28 +32,22 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
-
 // config de jwt
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
 {
-  options.RequireHttpsMetadata = false;
-  options.SaveToken = true;
-  options.TokenValidationParameters = new TokenValidationParameters()
-  {
-    ValidateIssuer = true,
-    ValidateAudience = true,
-    ValidateLifetime = true,
-    ValidateIssuerSigningKey = true,
-    ValidAudience = builder.Configuration["Jwt:Audience"],
-    ValidIssuer = builder.Configuration["Jwt:Issuer"],
-    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Token"]))
-  };
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Token"]))
+    };
 });
-
-
-
-
 
 builder.Services.AddAutoMapper(typeof(Program));
 
@@ -62,8 +56,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-  app.UseSwagger();
-  app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseCors();
